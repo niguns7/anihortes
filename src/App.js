c0bd0dd1f974897367ad1b03cf820828e6b356ea
nirguns7 <nirja767@gmail.com>
2022-10-02 14:57:18 +0545

    prvious button added

--- src/App.js
+++ src/App.js
@@ -22,12 +22,26 @@ class App extends Component {
       ]
     })
   }
+
+  prevHandler = () => {
+    //console.log("pressed");
+    this.setState({
+      person: [
+        {name: 'Nirgun', age: '28'},
+        {name: 'Pooja', age: '30'},
+        {name: 'anushree', age: '2'}
+      ]
+    })
+  }
   render() {
   return (
     <div className="App">
       <h1>Hi, i'm a react app</h1>
       <p>This is really working</p> 
+      <div className='button'>
       <button onClick={this.switchNameHandler}> Switch </button>
+      <button onClick={this.prevHandler}> prev </button>
+      </div>
       <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
       <Person name={this.state.person[1].name} age={this.state.person[1].age}> I am beautifull </Person>
       <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
