65687c930b3e0b9b81cfa2ec69a10e0862ad4302
nirguns7 <nirja767@gmail.com>
2022-10-03 13:10:18 +0545

    binding and call by reference

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { eventWrapper } from '@testing-library/user-event/dist/utils';
 import React, { useState } from 'react';
 import './App.css';
 import Person from './Person';
@@ -16,10 +17,10 @@ const [ personState, setPersonState] = useState ({
 
 console.log(personState);
 
- const switchNameHandler = () => {
+ const switchNameHandler = (newName) => {
   setPersonState({
     person: [
-      {name: 'Nirgun', age: '30'},
+      {name: newName, age: '30'},
       {name: 'Pooja', age: '32'},
       {name: 'anushree', age: '28'}
     ]
@@ -27,16 +28,39 @@ console.log(personState);
 };
 
 
+const switchNameHandler2 = () => {
+  setPersonState({
+    person: [
+      {name: 'Nirgun', age: '28'},
+      {name: 'Pooja', age: '30'},
+      {name: 'anushree', age: '26'}
+    ]
+  });
+};
+
+
+const namechangeHandler = (event) => {
+  setPersonState({
+    person: [
+      {name: 'Nirgun', age: '28'},
+      {name: event.target.value, age: '30'},
+      {name: 'anushree', age: '26'}
+    ]
+  });
+};
+
   return (
     <div className="App">
       <h1>Hi, i'm a react app</h1>
       <p>This is really working</p> 
       <div className='button'>
-      <button onClick={switchNameHandler}> Switch </button>
-      <button onClick={personState}> prev </button>
+      <button onClick={switchNameHandler.bind(this, 'NIRGUN')}> Switch </button>
+      <button onClick={switchNameHandler2}> prev </button>
       </div>
       <Person name={personState.person[0].name} age={personState.person[0].age}/>
-      <Person name={personState.person[1].name} age={personState.person[1].age}> I am beautifull </Person>
+      <Person name={personState.person[1].name} age={personState.person[1].age} 
+      click={switchNameHandler .bind(this, 'POOJA')}
+      changed={namechangeHandler}> I am beautifull </Person>
       <Person name={personState.person[2].name} age={personState.person[2].age}/>
     </div>
     
