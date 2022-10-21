10deec084d812ad80b2c8379d546751cd5164262
nirguns7 <nirja767@gmail.com>
2022-10-21 17:05:23 +0545

    project name changed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from '../src/containers/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App appTitle="Person Manager" Des="This is a person manager react application"/>
   </React.StrictMode>
 );
 
